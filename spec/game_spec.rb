0d32ca9a68365ceeb9a786b5c40272a5b61a26e1
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-06-15 20:43:27 +0100

    written tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,24 @@
 require 'game'
 
 describe Game do
+  xit 'knows if we are playing RPS or RPSSL' do
+  end
+
+  xit 'can be played by 1 or 2 players' do
+  end
+
+  xit 'cannot be player by more than 2 players' do
+  end
+
+  xit 'knows that if there is only one player online, the 2nd player is the CPU' do
+  end
+
+  xit 'chooses a random hand if there is only one player online' do
+  end
+
+  xit 'knows who the winner of a game is' do
+  end
+
+  xit 'knows when it\'s a draw' do
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,4 +1,12 @@
 require 'player'
 
 describe Player do
+  xit 'has a name' do
+  end
+
+  xit 'can choose a hand to play' do
+  end
+
+  xit 'cannot choose a hand that is not a RPSSL hand' do
+  end
 end
