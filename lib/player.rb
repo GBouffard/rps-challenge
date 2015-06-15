d341f7a9a7396f56cb72c1221850fd5e490d2fc3
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-06-15 21:13:50 +0100

    Player can choose a hand to play

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,10 @@
 class Player
-  attr_reader :name
+  attr_reader :name, :hand
   def initialize(name)
     @name = name
+  end
+
+  def choice(hand)
+    @hand = hand
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,12 +1,14 @@
 require 'player'
 
 describe Player do
-  player = Player.new('Guillaume')
+  let(:player) { Player.new('Guillaume') }
   it 'has a name' do
     expect(player.name).to eq 'Guillaume'
   end
 
-  xit 'can choose a hand to play' do
+  it 'can choose a hand to play' do
+    player.choice('rock')
+    expect(player.hand).to eq 'rock'
   end
 
   xit 'cannot choose a hand that is not a RPSSL hand' do
