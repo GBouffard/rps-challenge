a7cade8d38cec1aaf4fb3f3eb61f26982b47a051
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-06-17 12:11:58 +0100

    feature test - a 1 player RPSSL game vs CPU

--- spec/feature/playing_games_spec.rb
+++ spec/feature/playing_games_spec.rb
@@ -10,7 +10,11 @@ feature 'grand finale feature test - playing games' do
     expect(rps_game.decision(guillaume)).to satisfy { guillaume || 'DRAW!' || 'CPU' }
   end
 
-  xscenario 'a 1 player RPSSL game vs CPU' do
+  scenario 'a 1 player RPSSL game vs CPU' do
+    guillaume = Player.new('Guillaume')
+    rpssl_game = Game.new(guillaume)
+    guillaume.choice('spock')
+    expect(rpssl_game.decision(guillaume)).to satisfy { guillaume || 'DRAW!' || 'CPU' }
   end
 
   xscenario 'a 2 players RPS game' do
